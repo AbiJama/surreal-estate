1de36c4e3c3bcc758490f5a00c5770817cb7d3cf
Abi Jama <abi_jama88@hotmail.com>
2024-03-06 22:21:59 +0000

    created a navigation bar and added routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./NavBar";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
+import "../index.css";
 
-import React from 'react'
-
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h2>Surreal Estate</h2>
+      <NavBar />
+      <Routes>
+      <Route path="/Properties" element={<Properties />} />
+      <Route path="/AddProperty" element={<AddProperty />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import "../styles/navbar.css";
+
+const NavBar = () => {
+  const surrealLogoUrl = 'https://mcrcodes.s3.eu-west-2.amazonaws.com/course/surreal-estate/logo.png'
+
+  return (
+    <div className="navbar">
+      <img src={surrealLogoUrl} alt="Surreal Logo" className="navbar-logo" />
+      <ul className="navbar-links">
+            <li className="navbar-links-item">
+              <NavLink to="/Properties">View Properties</NavLink>
+            </li>
+            <li className="navbar-links-item">
+              <NavLink to="/AddProperty">Add a Property</NavLink>
+            </li>
+          </ul>
+        </div>
+      );
+    }
+
+export default NavBar;
